18e4e940d9393f4abaf0e69a55bfa0f6eaeda081
AndreyOvchynnikov <andrey272015@gmail.com>
2023-11-26 18:00:35 +0200

    update style

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,13 @@ const ProductPage = lazy(() => import("./pages/ProductPage"));
 
 function App() {
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+function appHeight() {
+  const doc = document.documentElement
+  doc.style.setProperty('--vh', (window.innerHeight*.01) + 'px');
+}
+
+  window.addEventListener('resize', appHeight);
+  appHeight();
 
   return (
     <StateContext>
